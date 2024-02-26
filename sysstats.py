300885bd55a36a0247feecf624769707a6e49149
Wintermute <mpotter@willkill.us>
2024-02-26 17:36:44 -0600

    Just some basic test changes

--- sysstats.py
+++ sysstats.py
@@ -23,7 +23,7 @@ signal.signal(signal.SIGINT, signal_handler)
 # Stores an enumeration of all the connected USB HID devices
 en = Enumeration()
 # Return a list of devices based on the search parameters / Hardcoded to Apex 7
-devices = en.find(vid=0x1038, pid=0x1612, interface=1)
+devices = en.find(vid=0x1038, pid=0x161c, interface=1)
 if not devices:
     devices = en.find(vid=0x1038, pid=0x1618, interface=1)
 if not devices:
@@ -42,13 +42,15 @@ draw = ImageDraw.Draw(im)
 while(1):
     # use a truetype font
     draw.rectangle([(0,0),(128,40)], fill=0)
-    font = ImageFont.truetype("OpenSans-Regular.ttf", 12)
+    font = ImageFont.truetype("OpenSans-Regular.ttf", 9)
 
     cpu_freq, cpu_min, cpu_max = psutil.cpu_freq()
 
     draw.text((0, 0), "CPU: {:2.0f}%, {:2d} cores".format(psutil.cpu_percent(interval=1), psutil.cpu_count()), font=font, fill=255)
-    draw.text((0, 12), "CPU Freq: {:4.0f}MHz".format(cpu_freq), font=font, fill=255)
-    draw.text((0, 24), "Memory: {:2.0f}% Used".format(psutil.virtual_memory().percent), font=font, fill=255)
+    #draw.text((0, 10), "CPU Freq: {:4.0f}MHz".format(cpu_freq), font=font, fill=255)
+    draw.text((0, 10), "Swap: {0}MiB".format(psutil.swap_memory()[1]/1048576), font=font, fill=255)
+    draw.text((0, 20), "CPU temp: {0}C".format(psutil.sensors_temperatures()['coretemp'][0].current), font=font, fill=255)
+    draw.text((0, 30), "Load: {0}".format(round(psutil.getloadavg()[0], 3)), font=font, fill=255)
 
     data = im.tobytes()
     # Set up feature report package
